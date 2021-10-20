9e3de2e1304ab0a7f2a21a0622c554430196251b
Rodion Ryzhov <57228093+LuminoDiode@users.noreply.github.com>
2021-10-20 11:13:57 +0300

    Mvc set up

---
+++ Website/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace Website.Controllers
+{
+	public class HomeController:Controller
+	{
+		[HttpGet]
+		public ViewResult Index()
+		{
+			return View("Index", (new string[] { "Planning" }));
+
+		}
+	}
+}
