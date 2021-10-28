9e7e385c1dd77b4d79adbb512efdc570a024814e
Rodion Ryzhov <57228093+LuminoDiode@users.noreply.github.com>
2021-10-28 23:16:56 +0300

    ArticleService prototype

---
+++ Website/Services/ArticleService.cs
@@ -0,0 +1,49 @@
+﻿using HtmlAgilityPack;
+using System.Linq;
+using System.Collections.Generic;
+using Website.Repository.Models;
+using System;
+using Website.Repository;
+using Website.Models.ArticleModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Website.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Website.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website.Services
+{
+	public interface IArticleService
+	{
+		//IQueryable<DbArticle> GetAllDbArticles();
+		Task<Article> GetArticleById(int id);
+	}
+	public class ArticleService : IArticleService
+	{
+		private readonly IDbArticleRepository dbArticleRepository;
+
+		public ArticleService(IDbArticleRepository dbArticleRepository)
+		{
+			this.dbArticleRepository = dbArticleRepository;
+		}
+		public async Task<Article> GetArticleById(int id)
+		{
+			return (await this.dbArticleRepository.GetDbArticleById(id)).ToArticle();
+		}
+	}
+}
