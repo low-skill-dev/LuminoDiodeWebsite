8cf84f4e92808c3d3c6f84da3a4e5e9c0e5bc766
Rodion Ryzhov <57228093+LuminoDiode@users.noreply.github.com>
2021-10-28 23:17:39 +0300

    Minor changes

--- FastTestConsoleApp/Program.cs
+++ FastTestConsoleApp/Program.cs
@@ -5,19 +5,19 @@ using LuminoDiodeRandomDataGenerators;
 using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
+using Utf8Json;
+
 
 namespace FastTestConsoleApp
 {
-    internal class Program
-    {
-        /* This project is created for quick testing any methods by simple
+	internal class Program
+	{
+		/* This project is created for quick testing any methods by simple
          * putting its output to the console
          */
-        static void Main(string[] args)
-        {
-            ArticleImage testArticleImage = new ArticleImage() { String64Image = RandomDataGenerator.String(20) };
-            Article testArticle = new Article() { Title = "TestArticle", Id = 0, AuthorUserId = 0, Tags = new[] { "tag1", "tag2" }, BodyParts = (new IArticleBodyPart[] { testArticleImage }).ToList() };
-            Console.WriteLine(JsonSerializer.Serialize(testArticle));
-        }
-    }
+		static void Main(string[] args)
+		{
+
+		}
+	}
 }
