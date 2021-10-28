7d5a9327f108759257a9082764a0be64d2d1508b
Rodion Ryzhov <57228093+LuminoDiode@users.noreply.github.com>
2021-10-28 22:31:15 +0300

    Ready to use context. No custom code.

---
+++ Website/Repository/WebsiteDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Website.Repository.Models;
+
+namespace Website.Repository
+{
+	public class WebsiteContext:DbContext
+	{
+		public WebsiteContext(DbContextOptions<WebsiteContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<DbArticle> DbArticles;
+	}
+}
