48d87634b35965d91b023733a9e80ca8f70b4e7f
Rodion Ryzhov <65biso2@gmail.com>
2021-11-28 19:50:57 +0300

    RecentDocuments and DocumentSearch services added

---
+++ Website/Services/DocumentSearchService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Website.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+using Website.Models.DocumentModel;
+using FuzzySharp;
+
+namespace Website.Services
+{
+	/// <summary>
+	/// Scope service which processes user document search requests and passes this request to
+	/// FrequentRequestsSingletonService (tbd)
+	/// </summary>
+	public class DocumentSearchService
+	{
+		private IServiceScopeFactory DbContextScopeFactory;
+		public DocumentSearchService(IServiceScopeFactory DbContextScopeFactory/*,FrequentRequestsSingletonService*/)
+		{
+			this.DbContextScopeFactory = DbContextScopeFactory;
+		}
+		
+		public async Task<IQueryable<DbDocument>> ProceedRequest(string UserRequest)
+		{
+
+			var DocsIQ = this.DbContextScopeFactory.CreateScope().ServiceProvider.GetRequiredService<WebsiteContext>().DbDocuments;
+			return await Task<IQueryable<DbDocument>>.Run(() =>
+			{
+				// В некоторых строках может быть очень много похожих слов, поэтому сначала следует
+				// применить сортировку по токенам, а потом уже непосредственно сравнить названия.
+				// К тому же изначальную сортировку по всему запросу убивает порядок слов.
+				return DocsIQ.OrderBy(d => Fuzz.TokenSetRatio(d.Title, UserRequest)).ThenBy(d=> Fuzz.Ratio(d.Title,UserRequest));
+			});
+		}
+	}
+}
---
+++ Website/Services/RecentDocumentsBackgroundService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Website.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Website.Services
+{
+	/// <summary>
+	/// Singleton service which provides <B>five</B> recent posted documents by making requsts to db by the interval.
+	/// </summary>
+	public class RecentDocumentsBackgroundService : BackgroundService
+	{
+		private IServiceScopeFactory DbContextScopeFactory;
+		public int Interval_msec = 1000 * 60 * 1; // 1 min interval
+
+		public RecentDocumentsBackgroundService(IServiceScopeFactory DbContextScopeFactory)
+		{
+			this.DbContextScopeFactory = DbContextScopeFactory;
+		}
+
+		public List<Website.Models.DocumentModel.DbDocument> RecentDocuments { get; private set; }
+
+		protected async override Task ExecuteAsync(CancellationToken ct)
+		{
+			using (WebsiteContext context = this.DbContextScopeFactory
+				.CreateScope().ServiceProvider.GetRequiredService<WebsiteContext>())
+			{
+				while (!ct.IsCancellationRequested)
+				{
+					var NewRecent = context.DbDocuments.OrderByDescending(d => d.CreatedDateTime);
+					NewRecent.Skip(NewRecent.Count() - 5);
+					this.RecentDocuments = NewRecent.ToList();
+					await Task.Delay(this.Interval_msec);
+				}
+			}
+		}
+	}
+}
