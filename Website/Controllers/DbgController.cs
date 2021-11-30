3a0e34cae29c759bc1d8a9bf92fba773564c7456
Rodion Ryzhov <65biso2@gmail.com>
2021-11-30 08:51:15 +0300

    Viewing of data from Singleton<FrequentDocumentSearchRequests> added

--- Website/Controllers/DbgController.cs
+++ Website/Controllers/DbgController.cs
@@ -1,19 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Website.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+using Website.Models.DocumentModel;
 
 namespace Website.Controllers
 {
 	public class DbgController : Controller
 	{
 		private readonly Website.Repository.WebsiteContext context;
-
-		public DbgController(Website.Repository.WebsiteContext ctx)
+		private readonly Website.Services.FrequentSearchRequestsService freqServ;
+		public DbgController(Website.Repository.WebsiteContext ctx, Website.Services.FrequentSearchRequestsService freqServ)
 		{
 			this.context = ctx;
+			this.freqServ = freqServ;
 		}
 
 		public ViewResult Summary()
 		{
-			return View(model: (this.context.DbArticles.AsQueryable()));
+			return View(model: (this.context.DbDocuments.AsQueryable()));
+		}
+		public ViewResult Show1()
+		{
+			return View(freqServ.FrequentRequests.Select(x=> x.DocumentSearchServiceScope.Request + '\t'+x.Frequency.ToString()));
 		}
 	}
 }
