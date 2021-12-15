c610f2481738ce2ff64f03f4ac8748b663c4ab8c
Rodion Ryzhov <65biso2@gmail.com>
2021-12-15 14:44:59 +0300

    Minor changes

--- Website/Models/ProjectModel/Project.cs
+++ Website/Models/ProjectModel/Project.cs
@@ -1,4 +1,18 @@
-﻿namespace Website.Models.ProjectModel
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Website.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+using FuzzySharp;
+
+namespace Website.Models.ProjectModel
 {
 	public class Project
 	{
@@ -6,11 +20,32 @@
 		{
 			Common = 0
 		}
-		public int Id { get; set; }
+		public int? Id { get; set; }
 		public PROJECT_TYPE ProjectType { get; set; }
+		public string Name { get; set; }
 		public string ShortDescription { get; set; }
 		public int OwnerId { get; set; }
 		public int[] AdminsId { get; set; }
 		public int[] OrderedDocumentsId { get; set; }
+
+		public static Project GenerateRandom(Website.Repository.WebsiteContext documentsSourceContext)
+		{
+			Random rnd = new Random();
+
+			var Docs = documentsSourceContext.DbDocuments.Select(x=>x.Id).ToList();
+
+			var RandomlyShuffeledIndeces =Enumerable.Range(0,(Docs.Count/rnd.Next(1,5))).OrderBy(x=> rnd.Next());
+
+			return new Project
+			{
+				ProjectType = PROJECT_TYPE.Common,
+				Name = "I am a test project",
+				ShortDescription = "Test project desciption",
+				OwnerId = 1,
+				AdminsId = new int[0],
+				// Doc ID is never null cause of it has been loaded from DB
+				OrderedDocumentsId = RandomlyShuffeledIndeces.Select(x => Docs[x].Value).ToArray()
+			};
+		}
 	}
 }
