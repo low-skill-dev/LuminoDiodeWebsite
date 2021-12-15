c0e31563ffd81918f1658812853a5eb36f1da632
Rodion Ryzhov <65biso2@gmail.com>
2021-12-15 14:46:44 +0300

    Startup changes

--- Website/Program.cs
+++ Website/Program.cs
@@ -1,18 +1,15 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace Website
 {
 	public class Program
 	{
+		[DllImport("kernel32.dll")] public extern static bool AllocConsole();
 		public static void Main(string[] args)
 		{
+			AllocConsole();
 			CreateHostBuilder(args).Build().Run();
 		}
 
--- Website/Startup.cs
+++ Website/Startup.cs
@@ -1,37 +1,59 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Website.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Website.Services;
+
 
 namespace Website
 {
 	public class Startup
 	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		private IServiceCollection services;
+
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
+			services.AddCors();
+
+			DbContextOptions<WebsiteContext> dbContextOptions = new DbContextOptions<WebsiteContext>();
+
+			services.AddDbContext<WebsiteContext>(opts =>{
+				opts.UseNpgsql("Server=localhost;Database=LuminodiodeWebsiteDb1;Password=qwerty;username=postgres");});
+
+			services.AddSingleton<Website.Services.RecentDocumentsBackgroundService>();
+			services.AddSingleton<Website.Services.FrequentSearchRequestsService>();
+			services.AddScoped<Website.Services.DocumentSearchService>();
+			this.services = services;
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
 			}
+			#region StartServices
+			app.ApplicationServices.GetService<RecentDocumentsBackgroundService>().StartAsync(new System.Threading.CancellationToken());
+			app.ApplicationServices.GetService<FrequentSearchRequestsService>().StartAsync(new System.Threading.CancellationToken());
+			#endregion
 
+
+			app.UseStaticFiles();
+			//app.UseStatusCodePages();
 			app.UseRouting();
 
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapDefaultControllerRoute();
+				endpoints.MapControllerRoute(
+					name: "default", pattern: "{controller=Home}/{Action=Summary}/{Id?}");
 			});
 		}
 	}
