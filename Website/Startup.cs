201f5e331cf0d1576b6e48b051206bb727f773d5
Rodion Ryzhov <65biso2@gmail.com>
2021-12-16 00:28:32 +0300

    New services added, obsolete deleted

--- Website/Startup.cs
+++ Website/Startup.cs
@@ -9,27 +9,52 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Website.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Website.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+using Website.Models.DocumentModel;
+using Microsoft.Extensions.Configuration;
+using Website.Services.SettingsProviders;
 
 
 namespace Website
 {
 	public class Startup
 	{
-		private IServiceCollection services;
+		private IConfiguration configuration;
+		public Startup(IConfiguration config)
+		{
+			this.configuration=config;
+		}
 
+		private IServiceCollection services;
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
-			services.AddCors();
-
-			DbContextOptions<WebsiteContext> dbContextOptions = new DbContextOptions<WebsiteContext>();
-
-			services.AddDbContext<WebsiteContext>(opts =>{
-				opts.UseNpgsql("Server=localhost;Database=LuminodiodeWebsiteDb1;Password=qwerty;username=postgres");});
 
 			services.AddSingleton<Website.Services.RecentDocumentsBackgroundService>();
 			services.AddSingleton<Website.Services.FrequentSearchRequestsService>();
+			var AppSetProv = new AppSettingsProvider(this.configuration);
+			services.AddSingleton<AppSettingsProvider>(AppSetProv);
 			services.AddScoped<Website.Services.DocumentSearchService>();
+
+			DbContextOptions<WebsiteContext> dbContextOptions = new DbContextOptions<WebsiteContext>();
+
+			services.AddDbContext<WebsiteContext>(opts => {
+				opts.UseNpgsql(AppSetProv.ConnectionStringsP.DefaultNpgsqlConnection);
+			});
+
 			this.services = services;
 		}
 
@@ -40,7 +65,7 @@ namespace Website
 			{
 				app.UseDeveloperExceptionPage();
 			}
-			#region StartServices
+			 #region StartServices
 			app.ApplicationServices.GetService<RecentDocumentsBackgroundService>().StartAsync(new System.Threading.CancellationToken());
 			app.ApplicationServices.GetService<FrequentSearchRequestsService>().StartAsync(new System.Threading.CancellationToken());
 			#endregion
