9b36b2a4b3a1d706f9f034f329160f64536a1123
Rodion Ryzhov <65biso2@gmail.com>
2021-12-16 08:06:58 +0300

    debug page Show2 for testing AppSettingsProvider service added

--- Website/Controllers/DbgController.cs
+++ Website/Controllers/DbgController.cs
@@ -13,17 +13,22 @@ using System.Collections.Generic;
 using System;
 using System.Text.RegularExpressions;
 using Website.Models.DocumentModel;
+using Microsoft.Extensions.Configuration;
+using Website.Services;
+using Website.Services.SettingsProviders;
 
 namespace Website.Controllers
 {
 	public class DbgController : Controller
 	{
-		private readonly Website.Repository.WebsiteContext context;
-		private readonly Website.Services.FrequentSearchRequestsService freqServ;
-		public DbgController(Website.Repository.WebsiteContext ctx, Website.Services.FrequentSearchRequestsService freqServ)
+		private readonly WebsiteContext context;
+		private readonly FrequentSearchRequestsService freqServ;
+		private readonly AppSettingsProvider configuration;
+		public DbgController(Website.Repository.WebsiteContext ctx, Website.Services.FrequentSearchRequestsService freqServ, AppSettingsProvider _config)
 		{
 			this.context = ctx;
 			this.freqServ = freqServ;
+			this.configuration = _config;
 		}
 
 		public ViewResult Summary()
@@ -33,6 +38,16 @@ namespace Website.Controllers
 		public ViewResult Show1()
 		{
 			return View(freqServ.FrequentRequests.Select(x=> x.DocumentSearchServiceScope.Request + '\t'+x.Frequency.ToString()));
+		}
+		public ViewResult Show2()
+		{
+			return View(model:new string[] {new string(
+				nameof(configuration.FrequentSearchRequestsServiceSP.Interval_msec) + " : "
+				+ configuration.FrequentSearchRequestsServiceSP.Interval_msec.ToString()+ "\n"+
+				nameof(configuration.FrequentSearchRequestsServiceSP.Interval_numOfRecentRequests) + " : "
+				+ configuration.FrequentSearchRequestsServiceSP.Interval_numOfRecentRequests.ToString() + "\n" +
+				nameof(configuration.FrequentSearchRequestsServiceSP.Interval_updateNeededCheck_msec) + " : "
+				+ configuration.FrequentSearchRequestsServiceSP.Interval_updateNeededCheck_msec + "\n") });
 		}
 	}
 }
