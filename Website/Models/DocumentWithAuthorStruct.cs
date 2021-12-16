86caf71891574b59dc208fd1d44c9ee84ec9785b
Rodion Ryzhov <65biso2@gmail.com>
2021-12-16 12:08:39 +0300

    marked as obsolete

--- Website/Models/DocumentWithAuthorStruct.cs
+++ Website/Models/DocumentWithAuthorStruct.cs
@@ -1,5 +1,9 @@
-﻿namespace Website.Models
+﻿using System;
+using System.Runtime;
+
+namespace Website.Models
 {
+	[Obsolete(null,true)]
 	public struct DocumentWithAuthorStruct
 	{
 		public DocumentModel.Document Document { get; init; }
