31564f9a97084d4b4b7f7668aec794e07a921482
Rodion Ryzhov <65biso2@gmail.com>
2022-03-14 11:12:33 +0300

    warn supressed

--- Website/Models/ProjectsGroupModel/ProjectsGroup.cs
+++ Website/Models/ProjectsGroupModel/ProjectsGroup.cs
@@ -5,10 +5,14 @@ namespace Website.Models.ProjectsGroupModel
 {
 	public class ProjectsGroup
 	{
+		#pragma warning disable CS8618
+
 		public int Id { get; set; }
 		public User OwnerId { get; set; }
 		public User[] AdminsId { get; set; }
 		public Project[] OrderedProjectsId { get; set; }
 		public string ShortDescription { get; set; }
+
+		#pragma warning restore CS8618
 	}
 }
