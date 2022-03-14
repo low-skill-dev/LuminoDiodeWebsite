6d4c36e70910bd50a870add24634337539f02232
Rodion Ryzhov <65biso2@gmail.com>
2022-03-14 11:08:15 +0300

    Abstract controller for being inherited by controllers with authed user

---
+++ Website/Controllers/AControllerWithAuth.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Website.Repository;
+using Website.Services;
+
+namespace Website.Controllers
+{
+	public abstract class AControllerWithAuth : Controller
+	{
+		protected Website.Models.UserModel.User? AuthedUser { get; set; } = null;
+		private WebsiteContext DbCtx;
+		//protected Website.Models.UserModel.User? AuthedUser=null;
+		protected readonly SessionManager SM;
+		public AControllerWithAuth(SessionManager SM, WebsiteContext ctx)
+		{
+			this.DbCtx = ctx;
+			this.SM = SM;
+			/*
+			if (HttpContext.Current.Request.Cookies.ContainsKey(SessionManager.SessionIdCoockieName))
+			{
+				SessionInfo? Info;
+#pragma warning disable CS8604
+				SM.ValidateSession(Request.Cookies[SessionManager.SessionIdCoockieName], out Info);
+#pragma warning restore CS8604
+				this.AuthedUserId = (Info?.UserId) ?? null;
+			}
+			*/
+		}
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			base.OnActionExecuting(context);
+			if (Request.Cookies.ContainsKey(SessionManager.SessionIdCoockieName))
+			{
+				SessionInfo? Info;
+#pragma warning disable CS8604
+				SM.ValidateSession(Request.Cookies[SessionManager.SessionIdCoockieName], out Info);
+#pragma warning restore CS8604
+				this.AuthedUser = DbCtx.Users.Find(Info?.UserId) ?? throw new System.AggregateException("Session cotains user id which cannot be found in DB");
+				ViewBag.AuthedUser = this.AuthedUser;
+			}
+		}
+	}
+}
