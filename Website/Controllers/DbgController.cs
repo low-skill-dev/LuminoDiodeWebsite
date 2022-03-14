a46e6f1b38dce2b2a81e2090b046580c9ef53bb3
Rodion Ryzhov <65biso2@gmail.com>
2022-03-14 11:09:24 +0300

    Now controllers inherits AControllerWIthAuth

--- Website/Controllers/DbgController.cs
+++ Website/Controllers/DbgController.cs
@@ -1,16 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Website.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Website.Repository;
+using Website.Services;
 
 namespace Website.Controllers
 {
-	public class DbgController : Controller
+	public class DbgController : AControllerWithAuth
 	{
 		private readonly Website.Repository.WebsiteContext context;
 		private readonly Website.Services.FrequentSearchRequestsService freqServ;
-		public DbgController(Website.Repository.WebsiteContext ctx, Website.Services.FrequentSearchRequestsService freqServ)
+		public DbgController(Website.Repository.WebsiteContext ctx, Website.Services.FrequentSearchRequestsService freqServ, SessionManager SM) : base(SM, ctx)
 		{
 			this.context = ctx;
 			this.freqServ = freqServ;
+			//if(Request.Cookies.ContainsKey("SessionId"));
+			ViewBag["UserName"] = 123;
 		}
 
 		public ViewResult Summary()
