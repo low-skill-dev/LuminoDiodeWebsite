97307ed9cd3de9c68f9794e8ae1f340f45172b1d
Rodion Ryzhov <65biso2@gmail.com>
2022-03-27 22:40:34 +0300

    minor changes

--- Website/Models/AuthModel/LoginInfo.cs
+++ Website/Models/AuthModel/LoginInfo.cs
@@ -2,7 +2,6 @@
 {
 	public class LoginInfo
 	{
-		public string UserName { get; set; } //For registration only
 		public string EmailPlainText { get; set; }
 		public string PasswordPlainText { get; set; }
 	}
