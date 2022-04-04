6391d0ff3658cf26e4504f9f0b12b771b6f0fefc
Rodion Ryzhov <65biso2@gmail.com>
2022-04-04 23:46:50 +0300

    added models for registration stages

---
+++ Website/Models/UserModel/NameModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+
+namespace Website.Models.UserModel
+{
+	public class NameModel
+	{
+		[DataType(DataType.Text)]
+		[MinLength(1), MaxLength(50)]
+		[RegularExpression(@".*[\S]{1,50}.*")] // at least 1 non-space chars
+		[Required(ErrorMessage = "Please enter a correct user name")]
+		[Display(Name = "First name")]
+		public string FirstName { get; set; } = null!;
+
+		[DataType(DataType.Text)]
+		[MinLength(1), MaxLength(50)]
+		[RegularExpression(@".*[\S]{1,50}.*")] // at least 1 non-space chars
+		[Required(ErrorMessage = "Please enter a correct user name")]
+		[Display(Name ="Last name")]
+		public string LastName { get; set; } = null!;
+
+		public void TrimAllField()
+		{
+			this.FirstName = this.FirstName.Trim();
+			this.LastName = this.LastName.Trim();
+		}
+	}
+}
