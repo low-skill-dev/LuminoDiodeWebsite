1f83a876823da09688d10b849ce194daa9a66c1b
Rodion Ryzhov <65biso2@gmail.com>
2022-04-04 23:45:21 +0300

    Added redirection to registration stage page if registration is not completed

--- Website/Controllers/Abstact/AMyController.cs
+++ Website/Controllers/Abstact/AMyController.cs
@@ -15,15 +15,16 @@ namespace Website.Controllers
 	/// </summary>
 	public abstract class AMyController : Controller
 	{
-		protected readonly WebsiteContext DbCtx;
+		protected readonly WebsiteContext context;
 		protected readonly SessionManager SM;
 		protected readonly RequestsFromIpCounterService RC;
+
 		protected Website.Models.UserModel.User? AuthedUser { get; set; } = null;
 		protected readonly List<Website.Models.ViewModels.Alert> PageTopAlerts = new(1);
 		public AMyController(IServiceScopeFactory ScopeFactory)
 		{
 			var sp = ScopeFactory.CreateScope().ServiceProvider;
-			this.DbCtx = sp.GetRequiredService<WebsiteContext>();
+			this.context = sp.GetRequiredService<WebsiteContext>();
 			this.SM = sp.GetRequiredService<SessionManager>();
 			this.RC = sp.GetRequiredService<RequestsFromIpCounterService>();
 			ViewBag.AuthedUser = new Website.Models.UserModel.User { };
@@ -32,10 +33,14 @@ namespace Website.Controllers
 		}
 		public override void OnActionExecuting(ActionExecutingContext context)
 		{
+			// Returns Http 429 if too many requests
 			RC.CountRequest(context);
-			if (RC.IPAddressIsBanned(context)) context.Result = new StatusCodeResult(429); // Return Http429 if too many requests
+			if (RC.IPAddressIsBanned(context)) context.Result = new StatusCodeResult(429);
+
 			base.OnActionExecuting(context);
+
 			LoadSessionAndAuthedUser();
+			RedirectToRegistrationStepIfNeeded(context);
 		}
 		protected void LoadSessionAndAuthedUser()
 		{
@@ -45,7 +50,7 @@ namespace Website.Controllers
 #pragma warning disable CS8604
 				SM.ValidateSession(Request.Cookies[SessionManager.SessionIdCoockieName], out Info);
 #pragma warning restore CS8604
-				this.AuthedUser = DbCtx.Users.Find(Info?.UserId) ?? null /* throw new System.AggregateException("Session cotains user id which cannot be found in DB")*/;
+				this.AuthedUser = context.Users.Find(Info?.UserId) ?? null /* throw new System.AggregateException("Session cotains user id which cannot be found in DB")*/;
 				ViewBag.AuthedUser = this.AuthedUser;
 			}
 		}
@@ -55,6 +60,26 @@ namespace Website.Controllers
 				this.TempData["PageTopAlerts"] = this.PageTopAlerts;
 
 			this.PageTopAlerts.Add(alert);
+		}
+		protected void RedirectToRegistrationStepIfNeeded(ActionExecutingContext context)
+		{
+			if (this.AuthedUser != null && AuthedUser.RegistrationStage != Models.UserModel.User.REGISTRATION_STAGE.RegistrationCompleted)
+			{
+				var act = context.RouteData.Values["Action"];
+				var ctr = context.RouteData.Values["controller"];
+
+				if (AuthedUser.RegistrationStage == Models.UserModel.User.REGISTRATION_STAGE.EnteringName)
+				{
+					if (act is null || ctr is null || !act.Equals("RegistrationStageEnteringName") || !ctr.Equals("User"))
+						context.Result = RedirectToAction("RegistrationStageEnteringName", "User");
+				}
+				if (AuthedUser.RegistrationStage == Models.UserModel.User.REGISTRATION_STAGE.EnteringMetadata)
+				{
+					if (act is null || ctr is null || !act.Equals("RegistrationStageEnteringMetadata") || !ctr.Equals("User"))
+						context.Result = RedirectToAction("RegistrationStageEnteringMetadata", "User");
+				}
+
+			}
 		}
 	}
 }
