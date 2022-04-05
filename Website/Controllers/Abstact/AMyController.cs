40d32a90269dc72b6d57dbf9e82f4d4ec630883b
Rodion Ryzhov <65biso2@gmail.com>
2022-04-05 12:09:48 +0300

    Improved readability of RedirectToRegistrationStepIfNeeded method

--- Website/Controllers/Abstact/AMyController.cs
+++ Website/Controllers/Abstact/AMyController.cs
@@ -61,25 +61,54 @@ namespace Website.Controllers
 
 			this.PageTopAlerts.Add(alert);
 		}
+
+		private enum ForwardingTo
+		{
+			LogoutPage,
+			EnteringNamePage,
+			EnteringMetadataPage,
+			OtherPages
+		}
+		private static string UserCtrName = nameof(Website.Controllers.UserController).Replace("Controller", string.Empty);
 		protected void RedirectToRegistrationStepIfNeeded(ActionExecutingContext context)
 		{
-			if (this.AuthedUser != null && AuthedUser.RegistrationStage != Models.UserModel.User.REGISTRATION_STAGE.RegistrationCompleted)
+			if (AuthedUser == null) return; // RETURN; if no user authed
+
+			var act = context.RouteData.Values["Action"] ?? string.Empty;
+			var ctr = context.RouteData.Values["controller"] ?? string.Empty;
+
+			ForwardingTo CurrentDist=ForwardingTo.OtherPages;
+
+			var UserCtrName = nameof(Website.Controllers.UserController).Replace("Controller", string.Empty);
+			var LogoutActName = nameof(Website.Controllers.UserController.Logout);
+			var EntNameActName = nameof(Website.Controllers.UserController.RegistrationStageEnteringName);
+			var EntMetaActName = nameof(Website.Controllers.UserController.RegistrationStageEnteringMetadata);
+
+			if (ctr.Equals(UserCtrName))
 			{
-				var act = context.RouteData.Values["Action"];
-				var ctr = context.RouteData.Values["controller"];
-
-				if (AuthedUser.RegistrationStage == Models.UserModel.User.REGISTRATION_STAGE.EnteringName)
-				{
-					if (act is null || ctr is null || !act.Equals("RegistrationStageEnteringName") || !ctr.Equals("User"))
-						context.Result = RedirectToAction("RegistrationStageEnteringName", "User");
-				}
-				if (AuthedUser.RegistrationStage == Models.UserModel.User.REGISTRATION_STAGE.EnteringMetadata)
-				{
-					if (act is null || ctr is null || !act.Equals("RegistrationStageEnteringMetadata") || !ctr.Equals("User"))
-						context.Result = RedirectToAction("RegistrationStageEnteringMetadata", "User");
-				}
-
+				if (act.Equals(LogoutActName))
+					CurrentDist=ForwardingTo.LogoutPage; // could return here, but returning just anywhere is not good
+				else if (act.Equals(EntNameActName))
+					CurrentDist = ForwardingTo.EnteringNamePage;
+				else if (act.Equals(EntMetaActName))
+					CurrentDist = ForwardingTo.EnteringMetadataPage;
 			}
+
+			if (CurrentDist == ForwardingTo.LogoutPage) return; // RETURN; If trying to logout, let him
+
+			ForwardingTo NeededDist=ForwardingTo.OtherPages;
+
+			if (AuthedUser.RegistrationStage == Models.UserModel.User.REGISTRATION_STAGE.EnteringName)
+				NeededDist = ForwardingTo.EnteringNamePage;
+			else if (AuthedUser.RegistrationStage == Models.UserModel.User.REGISTRATION_STAGE.EnteringMetadata)
+				NeededDist = ForwardingTo.EnteringMetadataPage;
+
+			if(NeededDist == CurrentDist) return; // RETURN; already heading needed page
+
+			if(NeededDist == ForwardingTo.EnteringNamePage)
+				context.Result = RedirectToAction(EntNameActName, UserCtrName);
+			else if (NeededDist==ForwardingTo.EnteringMetadataPage)
+				context.Result= RedirectToAction(EntMetaActName, UserCtrName);
 		}
 	}
 }
