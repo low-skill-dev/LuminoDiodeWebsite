dd6f7ca8da038bfed6d03f8100a1ea004b873d83
Rodion Ryzhov <65biso2@gmail.com>
2022-04-06 15:54:51 +0300

    Regex minor fixes

--- Website/Models/UserModel/MetadataModel.cs
+++ Website/Models/UserModel/MetadataModel.cs
@@ -5,34 +5,35 @@ namespace Website.Models.UserModel
 {
 	public class MetadataModel
 	{
+		private const string PleaseCorrectStr = "Please enter a correct value";
+
 		[DataType(DataType.Text)]
 		[MinLength(5), MaxLength(3000)]
-		[RegularExpression(@".*\S{5}.*")] // at least 5 non-space chars
+		[RegularExpression(@".*\S{5}.*",ErrorMessage = PleaseCorrectStr)] // at least 5 non-space chars
 		[Display(Name ="About me")]
-		
 		public string? AboutMe { get; set; } = null!;
 
 		[DataType(DataType.Text)]
 		[MinLength(5), MaxLength(50)]
-		[RegularExpression(@"\s*[t][.][m][e][/]\S+\s*")] // *t.me/?
+		[RegularExpression(@"\s*[t][.][m][e][/]\S+\s*",ErrorMessage = PleaseCorrectStr)] // *t.me/?
 		[Display(Name = "t.me link")]
 		public string? TelegramLink { get; set; } = null!;
 
 		[DataType(DataType.Text)]
 		[MinLength(5), MaxLength(50)]
-		[RegularExpression(@"\s*[v][k][.][c][o][m][/]\S?")] // *vk.com/?
+		[RegularExpression(@"\s*[v][k][.][c][o][m][/]\S+\s*",ErrorMessage = PleaseCorrectStr)] // *vk.com/?
 		[Display(Name = "vk.com link")]
 		public string? VkLink { get; set; } = null!;
 
 		[DataType(DataType.Text)]
 		[MinLength(1), MaxLength(50)]
-		[RegularExpression(@".*\S{5}.*")] // at least 1 non-space chars
+		[RegularExpression(@".*\S{1}.*",ErrorMessage = PleaseCorrectStr)] // at least 1 non-space chars
 		[Display(Name = "City")]
 		public string? City { get; set; } = null!;
 
 		[DataType(DataType.PostalCode)]
 		[MinLength(3), MaxLength(10)]
-		[RegularExpression(@".*\S{3}.*")] // at least 3 non-space chars
+		[RegularExpression(@".*\S{3}.*",ErrorMessage = PleaseCorrectStr)] // at least 3 non-space chars
 		[Display(Name = "Postal code")]
 		public string? PostalCode { get; set; } = null!;
 	
--- Website/Models/UserModel/NameModel.cs
+++ Website/Models/UserModel/NameModel.cs
@@ -6,17 +6,19 @@ namespace Website.Models.UserModel
 {
 	public class NameModel
 	{
+		private const string PleaseCorrectStr = "Please enter a correct value";
+
 		[DataType(DataType.Text)]
 		[MinLength(1), MaxLength(50)]
-		[RegularExpression(@".*[\S]{1,50}.*")] // at least 1 non-space chars
-		[Required(ErrorMessage = "Please enter a correct user name")]
+		[RegularExpression(@".*\S{1}.*",ErrorMessage = PleaseCorrectStr)] // at least 1 non-space chars
+		[Required(ErrorMessage = PleaseCorrectStr)]
 		[Display(Name = "First name")]
 		public string FirstName { get; set; } = null!;
 
 		[DataType(DataType.Text)]
 		[MinLength(1), MaxLength(50)]
-		[RegularExpression(@".*[\S]{1,50}.*")] // at least 1 non-space chars
-		[Required(ErrorMessage = "Please enter a correct user name")]
+		[RegularExpression(@".*\S{1}.*",ErrorMessage = PleaseCorrectStr)] // at least 1 non-space chars
+		[Required(ErrorMessage = PleaseCorrectStr)]
 		[Display(Name ="Last name")]
 		public string LastName { get; set; } = null!;
 
