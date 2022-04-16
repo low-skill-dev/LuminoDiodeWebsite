eb2248d963880f96de40e022c415ba23e8cc1d18
Rodion Ryzhov <65biso2@gmail.com>
2022-04-16 11:51:55 +0300

    Added models for document creation and edition

--- Website/Models/DocumentModel/DocumentEdition.cs
+++ Website/Models/DocumentModel/DocumentEdition.cs
@@ -3,15 +3,18 @@ using System.Linq;
 
 namespace Website.Models.DocumentModel
 {
-	public class DocumentCreation
+	public class DocumentEdition
 	{
 		private const string PleaseCorrectStr = "Please enter a correct value";
 
+		[Required]
+		public int Id { get; set; }
+
 		[DataType(DataType.Text)]
 		[Required(ErrorMessage = PleaseCorrectStr)]
-		[MinLength(3),MaxLength(200)]
+		[MinLength(3), MaxLength(200)]
 		[RegularExpression(@".*\S{3}.*")]
-		[Display(Name="Title")]
+		[Display(Name = "Title")]
 		public string Title { get; set; } = null!;
 
 		[DataType(DataType.MultilineText)]
@@ -21,7 +24,7 @@ namespace Website.Models.DocumentModel
 		[Display(Name = "Document text")]
 		public string Text { get; set; } = null!;
 
-		public static DocumentCreation FromDocument(Website.Models.DocumentModel.Document Doc) =>
-			new DocumentCreation { Title = Doc.Title, Text = string.Concat(Doc.Paragraphs.Select(x => string.Concat(x.TextParts?.Select(y => y.Text)?? new string[0]))) };
+		public static DocumentEdition FromDocument(Website.Models.DocumentModel.Document Doc) =>
+			new DocumentEdition { Id=Doc.Id, Title = Doc.Title, Text = string.Concat(Doc.Paragraphs.Select(x => string.Concat(x.TextParts?.Select(y => y.Text) ?? new string[0]))) };
 	}
 }
